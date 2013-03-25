042a170c4cdce8d7837ff7479d51ba30325b4e11
antonytrupe <antony.trupe@gmail.com>
2013-03-25 13:36:09 -0400

    1.1.0-SNAPSHOT cleaned up formatting and import statements in MiniProfilerFilter.java and MiniProfilerServlet.java changed memcache lifetime from 30 seconds to 50 minutes added a web.xml file copied the tmpl.js, mini_profiler.js, and mini_profiler.css files to the webapp directory

--- src/main/java/ca/jimr/gae/profiler/MiniProfilerServlet.java
+++ src/main/java/ca/jimr/gae/profiler/MiniProfilerServlet.java
@@ -21,13 +21,20 @@
  */
 package ca.jimr.gae.profiler;
 
-import java.io.*;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.http.*;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
@@ -46,186 +53,176 @@ import com.google.appengine.tools.appstats.MiniProfilerAppstats;
  * <li>Serves the static resources that make up the profiler UI.
  * </ul>
  */
-public class MiniProfilerServlet extends HttpServlet
-{
-  private static final long serialVersionUID = 7906645907029238585L;
+public class MiniProfilerServlet extends HttpServlet {
+	private static final long serialVersionUID = 7906645907029238585L;
 
-  private static final String MAX_STACK_FRAMES_KEY = "maxStackFrames";
-  private static final String HTML_ID_PREFIX_KEY = "htmlIdPrefix";
-  private static final String RESOURCE_CACHE_HOURS_KEY = "resourceCacheHours";
+	private static final String MAX_STACK_FRAMES_KEY = "maxStackFrames";
+	private static final String HTML_ID_PREFIX_KEY = "htmlIdPrefix";
+	private static final String RESOURCE_CACHE_HOURS_KEY = "resourceCacheHours";
 
-  /**
-   * The maximum number of stack frames that should show up in Appstats RPC
-   * details. If this is null the whole stack trace will be shown.
-   */
-  private Integer maxStackFrames;
-  /**
-   * The prefix for all HTML element ids/classes used in the profiler UI. This
-   * must be the same value as the {@code htmlIdPrefix} field in
-   * {@link MiniProfilerFilter}.
-   */
-  private String htmlIdPrefix = "mp";
-  /**
-   * The number of hours that the static resources should be cached in the
-   * browser for.
-   */
-  private int resourceCacheHours = 0;
+	/**
+	 * The maximum number of stack frames that should show up in Appstats RPC
+	 * details. If this is null the whole stack trace will be shown.
+	 */
+	private Integer maxStackFrames;
+	/**
+	 * The prefix for all HTML element ids/classes used in the profiler UI. This
+	 * must be the same value as the {@code htmlIdPrefix} field in
+	 * {@link MiniProfilerFilter}.
+	 */
+	private String htmlIdPrefix = "mp";
+	/**
+	 * The number of hours that the static resources should be cached in the
+	 * browser for.
+	 */
+	private int resourceCacheHours = 0;
 
-  /**
-   * The loader that will load the static resources for the profiler UI from
-   * files in the classpath.
-   */
-  private MiniProfilerResourceLoader resourceLoader;
-  /** Map of string replacements that will be done on loaded resources. */
-  private Map<String, String> resourceReplacements = new HashMap<String, String>();
-  /** The Appengine Memcache Service */
-  private MemcacheService ms;
+	/**
+	 * The loader that will load the static resources for the profiler UI from
+	 * files in the classpath.
+	 */
+	private MiniProfilerResourceLoader resourceLoader;
+	/** Map of string replacements that will be done on loaded resources. */
+	private Map<String, String> resourceReplacements = new HashMap<String, String>();
+	/** The Appengine Memcache Service */
+	private MemcacheService ms;
 
-  @Override
-  public void init(ServletConfig config) throws ServletException
-  {
-    String configMaxStackFrames = config.getInitParameter(MAX_STACK_FRAMES_KEY);
-    if (!isEmpty(configMaxStackFrames))
-    {
-      maxStackFrames = Integer.valueOf(configMaxStackFrames);
-    }
-    String configHtmlIdPrefix = config.getInitParameter(HTML_ID_PREFIX_KEY);
-    if (!isEmpty(configHtmlIdPrefix))
-    {
-      htmlIdPrefix = configHtmlIdPrefix.trim();
-    }
-    String configResourceCacheHours = config.getInitParameter(RESOURCE_CACHE_HOURS_KEY);
-    if (!isEmpty(configResourceCacheHours))
-    {
-      resourceCacheHours = Integer.parseInt(configResourceCacheHours);
-    }
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		String configMaxStackFrames = config
+				.getInitParameter(MAX_STACK_FRAMES_KEY);
+		if (!isEmpty(configMaxStackFrames)) {
+			maxStackFrames = Integer.valueOf(configMaxStackFrames);
+		}
+		String configHtmlIdPrefix = config.getInitParameter(HTML_ID_PREFIX_KEY);
+		if (!isEmpty(configHtmlIdPrefix)) {
+			htmlIdPrefix = configHtmlIdPrefix.trim();
+		}
+		String configResourceCacheHours = config
+				.getInitParameter(RESOURCE_CACHE_HOURS_KEY);
+		if (!isEmpty(configResourceCacheHours)) {
+			resourceCacheHours = Integer.parseInt(configResourceCacheHours);
+		}
 
-    ms = MemcacheServiceFactory.getMemcacheService(MiniProfilerFilter.MEMCACHE_NAMESPACE);
-    resourceLoader = new MiniProfilerResourceLoader();
-    resourceReplacements.put("@@prefix@@", htmlIdPrefix);
-  }
+		ms = MemcacheServiceFactory
+				.getMemcacheService(MiniProfilerFilter.MEMCACHE_NAMESPACE);
+		resourceLoader = new MiniProfilerResourceLoader();
+		resourceReplacements.put("@@prefix@@", htmlIdPrefix);
+	}
 
-  @Override
-  protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-  {
-    String requestURI = req.getRequestURI();
-    if (requestURI.endsWith("results"))
-    {
-      doResults(req, resp);
-    } else if (requestURI.endsWith("resource"))
-    {
-      doResource(req, resp);
-    }
-  }
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		String requestURI = req.getRequestURI();
+		if (requestURI.endsWith("results")) {
+			doResults(req, resp);
+		} else if (requestURI.endsWith("resource")) {
+			doResource(req, resp);
+		}
+	}
 
-  /**
-   * Serve one of the static resources for the profiler UI.
-   */
-  private void doResource(HttpServletRequest req, HttpServletResponse resp) throws IOException
-  {
-    boolean success = true;
-    String resource = (String) req.getParameter("id");
-    if (!isEmpty(resource))
-    {
-      if (resource.endsWith(".js"))
-      {
-        resp.setContentType("text/javascript");
-      } else if (resource.endsWith(".css"))
-      {
-        resp.setContentType("text/css");
-      } else if (resource.endsWith(".html"))
-      {
-        resp.setContentType("text/html");
-      } else
-      {
-        resp.setContentType("text/plain");
-      }
+	/**
+	 * Serve one of the static resources for the profiler UI.
+	 */
+	private void doResource(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		boolean success = true;
+		String resource = (String) req.getParameter("id");
+		if (!isEmpty(resource)) {
+			if (resource.endsWith(".js")) {
+				resp.setContentType("text/javascript");
+			} else if (resource.endsWith(".css")) {
+				resp.setContentType("text/css");
+			} else if (resource.endsWith(".html")) {
+				resp.setContentType("text/html");
+			} else {
+				resp.setContentType("text/plain");
+			}
 
-      String contents = resourceLoader.getResource(resource, resourceReplacements);
-      if (contents != null)
-      {
-        if (resourceCacheHours > 0)
-        {
-          Calendar c = Calendar.getInstance();
-          c.add(Calendar.HOUR, resourceCacheHours);
-          resp.setHeader("Cache-Control", "public, must-revalidate");
-          resp.setHeader("Expires", new SimpleDateFormat("EEE, dd MMM yyyy HH:mm:ss zzz").format(c.getTime()));
-        } else
-        {
-          resp.setHeader("Cache-Control", "no-cache");
-        }
+			String contents = resourceLoader.getResource(resource,
+					resourceReplacements);
+			if (contents != null) {
+				if (resourceCacheHours > 0) {
+					Calendar c = Calendar.getInstance();
+					c.add(Calendar.HOUR, resourceCacheHours);
+					resp.setHeader("Cache-Control", "public, must-revalidate");
+					resp.setHeader("Expires",
+							new SimpleDateFormat(
+									"EEE, dd MMM yyyy HH:mm:ss zzz").format(c
+									.getTime()));
+				} else {
+					resp.setHeader("Cache-Control", "no-cache");
+				}
 
-        PrintWriter w = resp.getWriter();
-        w.print(contents);
-      } else
-      {
-        success = false;
-      }
-    }
-    if (!success)
-    {
-      resp.sendError(404);
-    }
-  }
+				PrintWriter w = resp.getWriter();
+				w.print(contents);
+			} else {
+				success = false;
+			}
+		}
+		if (!success) {
+			resp.sendError(404);
+		}
+	}
 
-  /**
-   * Generate the results for a set of requests in JSON format.
-   */
-  private void doResults(HttpServletRequest req, HttpServletResponse resp) throws IOException, JsonGenerationException, JsonMappingException
-  {
-    Map<String, Object> result = new HashMap<String, Object>();
+	/**
+	 * Generate the results for a set of requests in JSON format.
+	 */
+	private void doResults(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, JsonGenerationException, JsonMappingException {
+		Map<String, Object> result = new HashMap<String, Object>();
 
-    String requestIds = req.getParameter("ids");
-    if (!isEmpty(requestIds))
-    {
-      List<Map<String, Object>> requests = new ArrayList<Map<String, Object>>();
-      for (String requestId : requestIds.split(","))
-      {
-        requestId = requestId.trim();
-        @SuppressWarnings("unchecked")
-        Map<String, Object> requestData = (Map<String, Object>) ms.get(String.format(MiniProfilerFilter.MEMCACHE_KEY_FORMAT_STRING, requestId));
-        if (requestData != null)
-        {
-          Map<String, Object> request = new HashMap<String, Object>();
-          request.put("id", requestId);
-          request.put("redirect", requestData.get("redirect"));
-          request.put("requestURL", requestData.get("requestURL"));
-          request.put("timestamp", requestData.get("timestamp"));
-          request.put("profile", requestData.get("profile"));
-          if (requestData.containsKey("appstatsId"))
-          {
-            Map<String, Object> appstatsMap = MiniProfilerAppstats.getAppstatsDataFor((String) requestData.get("appstatsId"), maxStackFrames);
-            request.put("appstats", appstatsMap != null ? appstatsMap : null);
-          } else
-          {
-            request.put("appstats", null);
-          }
-          requests.add(request);
-        }
-      }
-      result.put("ok", true);
-      result.put("requests", requests);
-    } else
-    {
-      result.put("ok", false);
-    }
+		String requestIds = req.getParameter("ids");
+		if (!isEmpty(requestIds)) {
+			List<Map<String, Object>> requests = new ArrayList<Map<String, Object>>();
+			for (String requestId : requestIds.split(",")) {
+				requestId = requestId.trim();
+				@SuppressWarnings("unchecked")
+				Map<String, Object> requestData = (Map<String, Object>) ms
+						.get(String.format(
+								MiniProfilerFilter.MEMCACHE_KEY_FORMAT_STRING,
+								requestId));
+				if (requestData != null) {
+					Map<String, Object> request = new HashMap<String, Object>();
+					request.put("id", requestId);
+					request.put("redirect", requestData.get("redirect"));
+					request.put("requestURL", requestData.get("requestURL"));
+					request.put("timestamp", requestData.get("timestamp"));
+					request.put("profile", requestData.get("profile"));
+					if (requestData.containsKey("appstatsId")) {
+						Map<String, Object> appstatsMap = MiniProfilerAppstats
+								.getAppstatsDataFor(
+										(String) requestData.get("appstatsId"),
+										maxStackFrames);
+						request.put("appstats",
+								appstatsMap != null ? appstatsMap : null);
+					} else {
+						request.put("appstats", null);
+					}
+					requests.add(request);
+				}
+			}
+			result.put("ok", true);
+			result.put("requests", requests);
+		} else {
+			result.put("ok", false);
+		}
 
-    resp.setContentType("application/json");
-    resp.setHeader("Cache-Control", "no-cache");
+		resp.setContentType("application/json");
+		resp.setHeader("Cache-Control", "no-cache");
 
-    ObjectMapper jsonMapper = new ObjectMapper();
-    jsonMapper.writeValue(resp.getOutputStream(), result);
-  }
+		ObjectMapper jsonMapper = new ObjectMapper();
+		jsonMapper.writeValue(resp.getOutputStream(), result);
+	}
 
-  /**
-   * Get whether the specified string is null or empty.
-   * 
-   * @param str
-   *          The string to test.
-   * @return Whether the string is empty.
-   */
-  private static boolean isEmpty(String str)
-  {
-    return str == null || str.trim().length() == 0;
-  }
+	/**
+	 * Get whether the specified string is null or empty.
+	 * 
+	 * @param str
+	 *            The string to test.
+	 * @return Whether the string is empty.
+	 */
+	private static boolean isEmpty(String str) {
+		return str == null || str.trim().length() == 0;
+	}
 }
